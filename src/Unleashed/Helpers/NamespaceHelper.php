ef95302aeb61c06b8bb2ce91248e7a47b34866e1
Colin O'Dell <colinodell@gmail.com>
2021-10-20 15:59:39 -0400

    Cache first namespace pointer for faster performance

---
+++ src/Unleashed/Helpers/NamespaceHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unleashed\Helpers;
+
+use PHP_CodeSniffer\Files\File;
+
+/**
+ * @internal
+ *
+ * Adapted from https://github.com/slevomat/coding-standard/blob/c7d4801da5b439cec0d7cd6fa770164b07a4d92b/SlevomatCodingStandard/Helpers/NamespaceHelper.php
+ */
+final class NamespaceHelper
+{
+    public static function getFirstNamespacePointer(File $phpcsFile): ?int
+    {
+        $lazyValue = static function () use ($phpcsFile): ?int {
+            $token = $phpcsFile->findNext(T_NAMESPACE, 0);
+
+            return $token === false ? null : $token;
+        };
+
+        return SniffLocalCache::getAndSetIfNotCached($phpcsFile, 'firstNamespacePointer', $lazyValue);
+    }
+}
